be55a937a57436548ddfb1bd324bc1e9a9e828e0
shaozu <shaozu.cao@gmail.com>
2021-07-26 16:32:31 +0800

    * fix a memory issue

--- vins_estimator/src/rosNodeTest.cpp
+++ vins_estimator/src/rosNodeTest.cpp
@@ -249,10 +249,18 @@ int main(int argc, char **argv)
 
     registerPub(n);
 
-    ros::Subscriber sub_imu = n.subscribe(IMU_TOPIC, 2000, imu_callback, ros::TransportHints().tcpNoDelay());
+    ros::Subscriber sub_imu;
+    if(USE_IMU)
+    {
+        sub_imu = n.subscribe(IMU_TOPIC, 2000, imu_callback, ros::TransportHints().tcpNoDelay());
+    }
     ros::Subscriber sub_feature = n.subscribe("/feature_tracker/feature", 2000, feature_callback);
     ros::Subscriber sub_img0 = n.subscribe(IMAGE0_TOPIC, 100, img0_callback);
-    ros::Subscriber sub_img1 = n.subscribe(IMAGE1_TOPIC, 100, img1_callback);
+    ros::Subscriber sub_img1;
+    if(STEREO)
+    {
+        sub_img1 = n.subscribe(IMAGE1_TOPIC, 100, img1_callback);
+    }
     ros::Subscriber sub_restart = n.subscribe("/vins_restart", 100, restart_callback);
     ros::Subscriber sub_imu_switch = n.subscribe("/vins_imu_switch", 100, imu_switch_callback);
     ros::Subscriber sub_cam_switch = n.subscribe("/vins_cam_switch", 100, cam_switch_callback);
